2143493c38e1d30366ad7fecc13ff571d871d6f6
sunkay <sunkay11@gmail.com>
2016-05-08 09:37:26 -0400

    adding cors middleware

--- server/index.js
+++ server/index.js
@@ -7,12 +7,14 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 //DB setup
 mongoose.connect('mongodb://localhost/auth');
 
 // App setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
