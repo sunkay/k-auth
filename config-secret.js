9bd5ed8ae10dba26c9a0da402b04e6aad5cc8b0c
sunkay <sunkay11@gmail.com>
2016-05-22 19:13:04 -0400

    TODO: load secret from backend service endpoint

---
+++ config-secret.js
@@ -0,0 +1,7 @@
+// hold application secrets and config
+// TODO: load the secret from a secure service endpoint instead
+// of storing it in a file. 
+
+module.exports = {
+  secret: "askdlmnz234cx897234dfsdaf"
+}
